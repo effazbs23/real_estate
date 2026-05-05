c5e01cccfe3908145cec6b565207a7c4e9fe05a7
effaz <effaz.rayhan@brainstation-23.com>
2026-05-05 17:45:23 +0600

    [ADD] estate: Added the estate module

---
+++ custom_addons/estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Real Estate Demo 223',
+    'version': '1.12',
+    'depends': ['base'],
+    'author': 'effazbs23',
+    'category': 'Sales',
+    'description': """
+        A module to manage real estate advertisements.
+    """,
+    'application': True,
+    'installable': True,
+}
