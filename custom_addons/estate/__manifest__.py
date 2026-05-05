bd9445e17a46d060e22bce54286daab985d6a0b7
effaz <effaz.rayhan@brainstation-23.com>
2026-05-05 19:25:19 +0600

    [IMP] estate: updated author on manifest

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -2,9 +2,10 @@
     'name': 'Real Estate',
     'version': '1.0',
     'depends': ['base'],
-    'author': 'brainstation-23',
+    'author': 'BRAIN STATION 23',
     'description': """
         A module to manage real estate advertisements.
     """,
+    'Application': True,
     'installable': True,
 }
