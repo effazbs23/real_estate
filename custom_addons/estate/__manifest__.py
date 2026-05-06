f62dadd1942a652f26143c6de5d7ab07e1cf03ee
effaz <effaz.rayhan@brainstation-23.com>
2026-05-06 17:01:24 +0600

    [ADD] estate: Added ir.model.access into manifest

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -2,6 +2,7 @@
     'name': 'Real Estate',
     'version': '1.0',
     'depends': ['base'],
+    'data':['security/ir.model.access.csv'],
     'author': 'BRAIN STATION 23',
     'description': """
         A module to manage real estate advertisements.
