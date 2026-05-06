7bee3d96ef88ac717f2ba07979a104c5f49615ee
effaz <effaz.rayhan@brainstation-23.com>
2026-05-06 18:42:27 +0600

    [FIX] estate: fixed estate_menus

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Real Estate',
     'version': '1.0',
     'depends': ['base'],
-    'data':['security/ir.model.access.csv', 'views/estate_property_views.xml'],
+    'data':['security/ir.model.access.csv', 'views/estate_property_views.xml', 'views/estate_menus.xml',],
     'author': 'BRAIN STATION 23',
     'description': """
         A module to manage real estate advertisements.
