6012dfd4d3e320ab3b3783bf9c56d18fa3168427
effaz <effaz.rayhan@brainstation-23.com>
2026-05-06 19:26:25 +0600

    [ADD] estate: Set default availability date 3 months from now and bedroom number to 2

--- custom_addons/estate/models/estate_property.py
+++ custom_addons/estate/models/estate_property.py
@@ -1,14 +1,16 @@
 from odoo import fields, models
+from datetime import date
+from dateutil.relativedelta import relativedelta
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate Property'
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(string="Available From", copy=False)
+    date_availability = fields.Date(string="Available From", copy=False, default=lambda self: date.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(string="Selling Price", copy=False, readonly=True)
-    bedrooms = fields.Integer()
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
