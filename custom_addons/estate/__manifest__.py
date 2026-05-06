5444abbc4ca0cd16a69bf89640f96d0d7c1c8a07
effaz <effaz.rayhan@brainstation-23.com>
2026-05-06 18:14:28 +0600

    [ADD] estate: Added estate_property_views inside manifest file

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Real Estate',
     'version': '1.0',
     'depends': ['base'],
-    'data':['security/ir.model.access.csv'],
+    'data':['security/ir.model.access.csv', 'views/estate_property_views.xml'],
     'author': 'BRAIN STATION 23',
     'description': """
         A module to manage real estate advertisements.
