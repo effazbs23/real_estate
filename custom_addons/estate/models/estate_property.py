4e3583fb08c064b83e9ef7a2fbdd084b89c8c7f6
effazrayhan <bsse1501@iit.du.ac.bd>
2026-05-07 14:58:25 +0600

    [ADD] estate: Added active field in manifest

--- custom_addons/estate/models/estate_property.py
+++ custom_addons/estate/models/estate_property.py
@@ -7,6 +7,7 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
+    active = fields.Boolean(string="active", default=True)
     date_availability = fields.Date(string="Available From", copy=False, default=lambda self: date.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(string="Selling Price", copy=False, readonly=True)
