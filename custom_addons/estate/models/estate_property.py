19acb443a0aee583f887fd7b353ab9e00fd7ecf4
effaz <effaz.rayhan@brainstation-23.com>
2026-05-11 19:43:43 +0600

    [ADD] estate: Added Best price tracking computation logic

--- custom_addons/estate/models/estate_property.py
+++ custom_addons/estate/models/estate_property.py
@@ -1,5 +1,5 @@
 
-from odoo import fields,models
+from odoo import fields, models, api
 from datetime import date
 from dateutil.relativedelta import relativedelta
 
@@ -44,3 +44,22 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     tag_ids = fields.Many2many("estate.property.tag",string="Tags")
     offer_ids = fields.One2many("estate.property.offer","property_id",string="Offers")
+    total_area = fields.Integer(string="Total Area",compute="_compute_area")
+
+    @api.depends("living_area","garden_area")
+    def _compute_area(self):
+        for record in self:
+            record.total_area= record.living_area + record.garden_area
+
+    best_price = fields.Integer(string="Best Price", compute="_compute_best_price")
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            prices = record.offer_ids.mapped("price")
+        if prices:
+            record.best_price = max(prices)
+        else:
+            record.best_price = 0.0
+
+
