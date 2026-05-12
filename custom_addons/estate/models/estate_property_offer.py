6691b84e1bbb70fe714a3c861b5a665941148539
effaz <effaz.rayhan@brainstation-23.com>
2026-05-12 12:38:54 +0600

    [ADD] estate: Added validity date with inverse computation

--- custom_addons/estate/models/estate_property_offer.py
+++ custom_addons/estate/models/estate_property_offer.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+import datetime as dt
+
+from odoo import fields, models,api
 
 
 class EstatePropertyOffer(models.Model):
@@ -10,5 +12,18 @@ class EstatePropertyOffer(models.Model):
         ('accepted', 'Accepted'),
         ('refused', 'Refused')]
         ,copy=False)
+    validity = fields.Integer(default=7)
     partner_id = fields.Many2one('res.partner', required=True, string="Partner ID")
     property_id = fields.Many2one('estate.property', required=True, string="Property ID")
+    date_deadline = fields.Date(string="Deadline", compute="_compute_deadline",inverse="_compute_deadline_inverse")
+    @api.depends("validity", "create_date")
+    def _compute_deadline(self):
+        for record in self:
+            base_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.date_deadline = base_date + dt.timedelta(days=record.validity)
+
+    @api.depends("validity","create_date")
+    def _compute_deadline_inverse(self):
+        for record in self:
+            base_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.validity = (record.date_deadline - base_date).days
