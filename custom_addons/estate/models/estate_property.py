72e623401c80c2c507280f94d6b383627e2f00a9
effaz <effaz.rayhan@brainstation-23.com>
2026-05-13 14:39:59 +0600

    [REM] estate: Removed redundant line of property_type_id

--- custom_addons/estate/models/estate_property.py
+++ custom_addons/estate/models/estate_property.py
@@ -58,7 +58,6 @@ class EstateProperty(models.Model):
     salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
-    type_ids = fields.Many2many('estate.property.type', String="Types")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
     total_area = fields.Integer(string="Total Area", compute="_compute_area")
 
