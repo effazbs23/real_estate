62387d8615eb531da92df6f063171288ffaa4248
effaz <effaz.rayhan@brainstation-23.com>
2026-05-15 19:27:00 +0600

    [ADD] estate_account: Added blank invoice generation using accounts.move

--- custom_addons/estate_account/models/estate_property.py
+++ custom_addons/estate_account/models/estate_property.py
@@ -8,5 +8,9 @@ class EstateProperty(models.Model):
 
     def action_sell(self):
         act = super().action_sell()
+        self.env["account.move"].create({
+            'partner_id':self.buyer_id.id,
+            'move_type':'out_invoice'
+        })
         print("--------------Sell Confirmed!!!----------------")
         return act
