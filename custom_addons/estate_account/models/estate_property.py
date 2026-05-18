33f0ced1535e1e2ca646a280498e09ff1d1ab6d3
effaz <effaz.rayhan@brainstation-23.com>
2026-05-18 11:26:24 +0600

    [ADD] estate_account: Added 6% of the selling price and an additional 100.00 from administrative fees to the invoice

--- custom_addons/estate_account/models/estate_property.py
+++ custom_addons/estate_account/models/estate_property.py
@@ -1,6 +1,5 @@
-
 from odoo import fields, models
-from odoo.exceptions import UserError
+from odoo import Command
 
 
 class EstateProperty(models.Model):
@@ -8,9 +7,21 @@ class EstateProperty(models.Model):
 
     def action_sell(self):
         act = super().action_sell()
-        self.env["account.move"].create({
-            'partner_id':self.buyer_id.id,
-            'move_type':'out_invoice'
-        })
+        self.env["account.move"].create({'partner_id': self.buyer_id.id,
+                                         'move_type': 'out_invoice',
+                                         "invoice_line_ids": [
+                                             Command.create({
+                                                 "name": f"{self.name}",
+                                                 "quantity": 1,
+                                                 "price_unit": self.selling_price * 0.06
+                                             }),
+                                             Command.create({
+                                                 "name": "Administrative Fees",
+                                                 "quantity": 1,
+                                                 "price_unit": 100
+                                             })
+                                         ]
+
+                                         })
         print("--------------Sell Confirmed!!!----------------")
         return act
