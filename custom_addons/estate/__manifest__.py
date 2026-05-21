8bc44d3616a37a5bdec4001730a032917dd69a0c
effaz <effaz.rayhan@brainstation-23.com>
2026-05-21 18:20:03 +0600

    [ADD] estate: Added Record Rules

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -8,6 +8,7 @@
     """,
     'data': [
         'security/ir.model.access.csv',
+        'security/security.xml',
         'data/estate_property_type_data.xml',
         'data/estate_inflation_cron.xml',
         'views/estate_property_views.xml',
