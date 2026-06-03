5d1d2020de5b37562a8b9481c5f573090b69c75f
effaz <effaz.rayhan@brainstation-23.com>
2026-06-03 13:14:43 +0600

    [ADD] expense_report: Added Excel report generation

--- custom_addons/expense_report/__manifest__.py
+++ custom_addons/expense_report/__manifest__.py
@@ -1,12 +1,13 @@
 {
     'name': 'Expense Report',
     'version': '1.0.0',
-    'depends': ['base','estate'],
+    'depends': ['base','estate','report_xlsx'],
     'author': 'BRAIN STATION 23',
     'description': """
         A module to generate pdf reports for real estate application.
     """,
     'data':[
+        'report/expense_reports_excel.xml',
         'report/expense_reports.xml',
         'report/expense_report_templates.xml',
     ],
