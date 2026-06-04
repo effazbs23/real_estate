f1e88c4a1be6ab3dac9e1ea33a9ca39ccbd9c210
effaz <effaz.rayhan@brainstation-23.com>
2026-06-04 19:17:30 +0600

    [ADD] awesome_owl: Added Props as cards

--- custom_addons/tutorials/awesome_owl/static/src/playground.js
+++ custom_addons/tutorials/awesome_owl/static/src/playground.js
@@ -2,10 +2,11 @@
 
 import { Component } from "@odoo/owl";
 import { Counter } from "@awesome_owl/counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
 
-    static components = { Counter };
+    static components = { Counter,Card };
 }
 
