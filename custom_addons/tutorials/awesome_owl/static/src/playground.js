93ca9aad4d089040db08496efec0d76c3e83e110
effaz <effaz.rayhan@brainstation-23.com>
2026-06-04 19:39:43 +0600

    [ADD] awesome_owl: Added t-out and markups

--- custom_addons/tutorials/awesome_owl/static/src/playground.js
+++ custom_addons/tutorials/awesome_owl/static/src/playground.js
@@ -1,12 +1,15 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component,markup } from "@odoo/owl";
 import { Counter } from "@awesome_owl/counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
 
-    static components = { Counter,Card };
+    static components = {Counter, Card}
+    setup() {
+        this.str1 = "<div class='text-primary'>content</div>";
+        this.str2 = markup("<div class='text-primary'>content</div>");
+    }
 }
-
