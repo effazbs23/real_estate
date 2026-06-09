fa26f2516df8ddbb2b0818da1b2f4e606ea04186
effaz <effaz.rayhan@brainstation-23.com>
2026-06-09 12:04:34 +0600

    [ADD] awesome_owl: Added static data

--- custom_addons/tutorials/awesome_owl/static/src/components/AttendanceDashboard/attendance_dashboard.js
+++ custom_addons/tutorials/awesome_owl/static/src/components/AttendanceDashboard/attendance_dashboard.js
@@ -1,6 +1,31 @@
 /** @odoo-module **/
-import { Component, useState } from "@odoo/owl";
+import {Component, useState} from "@odoo/owl";
 
-export class AttendanceDashboard extends Component{
+export class AttendanceDashboard extends Component {
     static template = "awesome_owl.attendanceDashboard";
+
+    setup() {
+        this.data = useState(
+            {
+                rows: [
+                    {
+                        id: 1,
+                        name: "John Doe",
+                        check_in: "2026-06-08 09:00:00",
+                        check_out: "2026-06-08 18:00:00",
+                        hours: 9
+                    },
+                    {
+                        id: 2,
+                        name: "Alice",
+                        check_in: "2026-06-08 08:30:00",
+                        check_out: "2026-06-08 17:30:00",
+                        hours: 9
+                    },
+                ]
+            }
+        );
+    }
+
+
 }
