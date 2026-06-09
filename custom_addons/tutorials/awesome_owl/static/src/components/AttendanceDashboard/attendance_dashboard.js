1d0b77557b92b5b74609d0df595fda09c4bfd1e3
effaz <effaz.rayhan@brainstation-23.com>
2026-06-09 11:55:29 +0600

    [ADD] awesome_owl: Added new component attendance dashboard and created a table

---
+++ custom_addons/tutorials/awesome_owl/static/src/components/AttendanceDashboard/attendance_dashboard.js
@@ -0,0 +1,6 @@
+/** @odoo-module **/
+import { Component, useState } from "@odoo/owl";
+
+export class AttendanceDashboard extends Component{
+    static template = "awesome_owl.attendanceDashboard";
+}
--- custom_addons/tutorials/awesome_owl/static/src/playground.js
+++ custom_addons/tutorials/awesome_owl/static/src/playground.js
@@ -1,20 +1,13 @@
 /** @odoo-module **/
 
 import { Component,markup,useState } from "@odoo/owl";
-import { Counter } from "@awesome_owl/counter/counter";
-import { Card } from "./card/card";
+import {AttendanceDashboard} from "./components/AttendanceDashboard/attendance_dashboard";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
 
-    static components = {Counter, Card}
+    static components = { AttendanceDashboard };
     setup() {
-        this.str1 = "<div class='text-primary'>content</div>";
-        this.str2 = markup("<div class='text-primary'>content</div>");
-        this.sum = useState({ value:2 })
-    }
 
-    incrementSum(){
-        this.sum.value++;
     }
 }
