ec254d3ff441093586369a43bdbf8388a224aa2c
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-12 11:10:50 +0600

    [ADD] awesome_clicker: Added the clicker

---
+++ custom_addons/tutorials/awesome_clicker/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Awesome Clicker",
+
+    'author': "BRAIN STATION 23",
+    'category': 'Tutorials',
+    'version': '1.0.0',
+    'application': True,
+    'installable': True,
+    'depends': ['base', 'web'],
+
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            'awesome_clicker/static/src/**/*',
+        ],
+
+    },
+    'license': 'AGPL-3'
+}
