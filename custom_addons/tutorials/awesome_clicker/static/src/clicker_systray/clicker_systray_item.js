ac4cc947c795bccbba58eb2c93af7d82e228706e
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-12 12:10:12 +0600

    [ADD] awesome_clicker: Added Client action and used service doAction

--- custom_addons/tutorials/awesome_clicker/static/src/clicker_systray/clicker_systray_item.js
+++ custom_addons/tutorials/awesome_clicker/static/src/clicker_systray/clicker_systray_item.js
@@ -1,5 +1,6 @@
 import { registry } from "@web/core/registry";
 import { Component, useState, useExternalListener } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
 
 export class ClickerSystray extends Component {
     static template = "awesome_clicker.ClickerSystray";
@@ -10,10 +11,19 @@ export class ClickerSystray extends Component {
         useExternalListener(document.body, "click", () => {
             this.state.counter++
         });
+        this.action = useService("action");
     }
 
     increment() {
         this.state.counter+= 9;
+    }
+    openClientAction(){
+        this.action.doAction({
+            type: "ir.actions.client",
+            tag: "awesome_clicker.client_action",
+            target: "new",
+            name: "Clicker"
+        });
     }
 
 }
---
+++ custom_addons/tutorials/awesome_clicker/static/src/client_action/client_action.js
@@ -0,0 +1,9 @@
+
+import { Component } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+
+export class ClientAction extends Component{
+    static template = "awesome_clicker.client_action";
+}
+
+registry.category("actions").add("awesome_clicker.client_action", ClientAction);
