b1d768358760a7b6251bf0e4f57f81e4ab46bdda
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-15 18:27:07 +0600

    [ADD] awesome_galllery: Added the boilerplates

---
+++ custom_addons/tutorials/awesome_gallery/static/src/gallery_controller.js
@@ -0,0 +1,7 @@
+/**odoo-module**/
+
+import { Component } from "@odoo/owl";
+
+export class GalleryController extends Component {
+    static template = "awesome_gallery.GalleryController";
+}
---
+++ custom_addons/tutorials/awesome_gallery/static/src/gallery_view.js
@@ -0,0 +1,13 @@
+/**odoo-module**/
+
+import { registry } from "@web/core/registry";
+import { GalleryController } from "./gallery_controller";
+
+export const galleryView = {
+    type: "gallery",
+    display_name: "Gallery",
+    icon: "oi oi-apps",
+    Controller: GalleryController,
+};
+
+registry.category("views").add("gallery", galleryView);
