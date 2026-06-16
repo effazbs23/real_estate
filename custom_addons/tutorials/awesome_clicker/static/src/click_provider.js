ea3731073df09fcf1a04c2e19f68b13a8f978abb
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-16 14:53:24 +0600

    [ADD] awesome_clicker: Added command to open clicker game and buy clickbots

---
+++ custom_addons/tutorials/awesome_clicker/static/src/click_provider.js
@@ -0,0 +1,29 @@
+
+import { registry } from "@web/core/registry";
+
+const commandProviderRegistry = registry.category("command_provider");
+commandProviderRegistry.add("clicker",
+    {
+        provide: (env,options) => {
+            return [
+                {
+                    name: "Buy 1 ClickBot",
+                    action(){
+                        env.services.clicker.buyClickBot();
+                    }
+                },
+                {
+                    name: "Open Clicker Game",
+                    action(){
+                        env.services.action.doAction({
+                            type: "ir.actions.client",
+                            tag:"awesome_clicker.client_action",
+                            target:"new",
+                            name:"Clicker Game"
+                        });
+                    }
+                }
+            ]
+        }
+    }
+);
