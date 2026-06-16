4a6eda053df2603359c789bada7888443e94030e
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-16 16:41:57 +0600

    [ADD] awesome_clicker: Added Dropdown Menu on systray

--- custom_addons/tutorials/awesome_clicker/static/src/clicker_systray/clicker_systray_item.js
+++ custom_addons/tutorials/awesome_clicker/static/src/clicker_systray/clicker_systray_item.js
@@ -1,19 +1,38 @@
+
 import { registry } from "@web/core/registry";
 import { Component, useState } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { useClicker } from "../use_clicker";
 import { ClickValue } from "../click_value/click_value";
+import { Dropdown } from "@web/core/dropdown/dropdown";
+import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 
 export class ClickerSystray extends Component {
     static template = "awesome_clicker.ClickerSystray";
     static props = {};
-    static components = { ClickValue };
+    static components = { ClickValue,Dropdown,DropdownItem };
 
     setup() {
 
         this.clicker = useClicker();
         this.action = useService("action");
     }
+    get treeNumber(){
+        let s=0;
+        for(const tree in this.clicker.trees){
+            s += this.clicker.trees[tree].number;
+        }
+        return s;
+    }
+
+    get fruitNumber(){
+        let s = 0;
+        for (const fruit in this.clicker.fruits){
+            s+= this.clicker.fruits[fruit];
+        }
+        return s;
+    }
+
 
     openClientAction(){
         this.action.doAction({
