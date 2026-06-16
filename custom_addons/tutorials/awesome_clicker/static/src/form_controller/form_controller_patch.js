3b42a8634192dc16d4fd2907526a9d3a425ea42d
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-16 17:59:19 +0600

    [FIX] awesome_clicker: Fixed the function call parameters

--- custom_addons/tutorials/awesome_clicker/static/src/form_controller/form_controller_patch.js
+++ custom_addons/tutorials/awesome_clicker/static/src/form_controller/form_controller_patch.js
@@ -1,12 +1,12 @@
 import { FormController } from "@web/views/form/form_controller";
 import { patch } from "@web/core/utils/patch";
-import {useClicker} from "../use_clicker";
+import { useService } from "@web/core/utils/hooks";
 
 const FormControllerPatch = {
     setup(){
         super.setup(...arguments);
+        const clicker = useService("clicker");
         if(Math.random() < 0.01 ){
-            const clicker = useClicker();
             clicker.getRewards();
         }
     }
