95a81cb0cda4f8ca0df8b7b028066d467cb51a31
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-16 17:29:59 +0600

    [ADD] awesome_clicker: Added Notebook views

--- custom_addons/tutorials/awesome_clicker/static/src/client_action/client_action.js
+++ custom_addons/tutorials/awesome_clicker/static/src/client_action/client_action.js
@@ -4,9 +4,10 @@ import { registry } from "@web/core/registry";
 import { useClicker } from "../use_clicker";
 import { ClickValue } from "../click_value/click_value";
 
+import { Notebook } from "@web/core/notebook/notebook";
 export class ClientAction extends Component{
     static template = "awesome_clicker.client_action";
-    static components = { ClickValue };
+    static components = { ClickValue, Notebook };
 
     setup(){
         this.clicker = useClicker();
