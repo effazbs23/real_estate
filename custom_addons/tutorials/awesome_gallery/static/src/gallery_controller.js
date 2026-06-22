a781a746c4ab18d4c1dddbf0b50a80def28de442
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-22 16:45:41 +0600

    [ADD] awesome_gallery: Added file size and ids in gallery view

--- custom_addons/tutorials/awesome_gallery/static/src/gallery_controller.js
+++ custom_addons/tutorials/awesome_gallery/static/src/gallery_controller.js
@@ -1,9 +1,34 @@
 /**odoo-module**/
 
-import { Component } from "@odoo/owl";
+import { Component, onWillStart, onWillUpdateProps, useState } from "@odoo/owl";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 export class GalleryController extends Component {
     static template = "awesome_gallery.GalleryController";
     static components = { Layout };
+
+    setup() {
+        this.orm = useService("orm");
+        this.state = useState({ images: [] });
+        const { domain, resModel } = this.props;
+        onWillStart(() => this.loadImages(domain));
+        onWillUpdateProps((nextProps) => this.loadImages(nextProps.domain));
+    }
+
+    async loadImages(domain) {
+        const { length, records } = await this.orm.webSearchRead(
+            this.props.resModel,
+            domain,
+            {
+                specification: {
+                    [this.props.archInfo.imageField]: {},
+                },
+                context: {
+                    bin_size: true,
+                },
+            },
+        );
+        this.state.images = records;
+    }
 }
