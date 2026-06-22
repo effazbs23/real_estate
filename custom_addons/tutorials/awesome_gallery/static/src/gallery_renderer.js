e9ff018f3b67bdfb0cdc6b0b333aa7e458af155b
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-22 17:42:27 +0600

    [ADD] awesome_gallery: Updated the renderer to display images

--- custom_addons/tutorials/awesome_gallery/static/src/gallery_renderer.js
+++ custom_addons/tutorials/awesome_gallery/static/src/gallery_renderer.js
@@ -1,7 +1,16 @@
 /**odoo-module**/
 
 import { Component } from "@odoo/owl";
+import { url } from "@web/core/utils/urls";
 
 export class GalleryRenderer extends Component {
     static template = "awesome_gallery.GalleryRenderer";
+
+    imageUrl(image) {
+        return url("/web/image", {
+            model: this.props.resModel,
+            id: image.id,
+            field: this.props.archInfo.imageField,
+        });
+    }
 }
