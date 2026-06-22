870e14bbddd763dc6c29a397f9f97954a7db6af7
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-22 17:54:39 +0600

    [ADD] awesome_gallery:  Added switch to form view on click

--- custom_addons/tutorials/awesome_gallery/static/src/gallery_controller.js
+++ custom_addons/tutorials/awesome_gallery/static/src/gallery_controller.js
@@ -10,9 +10,11 @@ export class GalleryController extends Component {
 
     setup() {
         const orm = useService("orm");
+        const action = useService("action");
         this.model = new this.props.Model(orm, this.props.resModel, this.props.archInfo);
         const { domain } = this.props;
         onWillStart(() => this.model.loadImages(domain));
         onWillUpdateProps((nextProps) => this.model.loadImages(nextProps.domain));
+        this.openRecord = (image) => action.switchView("form", { resId: image.id });
     }
 }
