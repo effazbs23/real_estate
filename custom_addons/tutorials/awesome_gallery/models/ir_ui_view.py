544d97cf3007f818d4666e890d75f0363943ca5c
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-22 18:48:19 +0600

    [ADD] awesome_gallery: Added Relax NG for validation

--- custom_addons/tutorials/awesome_gallery/models/ir_ui_view.py
+++ custom_addons/tutorials/awesome_gallery/models/ir_ui_view.py
@@ -1,5 +1,32 @@
 # -*- coding: utf-8 -*-
+import logging
+import os
+
+from lxml import etree
+
 from odoo import fields, models
+from odoo.loglevels import ustr
+from odoo.tools import misc, view_validation
+
+_logger = logging.getLogger(__name__)
+
+_gallery_validator = None
+
+
+@view_validation.validate('gallery')
+def schema_gallery(arch, **kwargs):
+    global _gallery_validator
+
+    if _gallery_validator is None:
+        with misc.file_open(os.path.join('awesome_gallery', 'rng', 'gallery_view.rng')) as f:
+            _gallery_validator = etree.RelaxNG(etree.parse(f))
+
+    if _gallery_validator.validate(arch):
+        return True
+
+    for error in _gallery_validator.error_log:
+        _logger.error(ustr(error))
+    return False
 
 
 class View(models.Model):
@@ -9,3 +36,8 @@ class View(models.Model):
 
     def _get_view_info(self):
         return {'gallery': {'icon': 'oi oi-apps'}} | super()._get_view_info()
+
+    def _validate_tag_gallery(self, node, name_manager, node_info):
+        for attr_name in ('image_field', 'tooltip_field'):
+            if field_name := node.get(attr_name):
+                name_manager.has_field(node, field_name, node_info)
