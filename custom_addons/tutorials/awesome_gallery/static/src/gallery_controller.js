2dc4f8667170ec3b0ccb7f9a0948d51014ab5cf7
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-22 16:29:50 +0600

    [ADD] awesome_gallery: Used the Layout component

--- custom_addons/tutorials/awesome_gallery/static/src/gallery_controller.js
+++ custom_addons/tutorials/awesome_gallery/static/src/gallery_controller.js
@@ -1,7 +1,9 @@
 /**odoo-module**/
 
 import { Component } from "@odoo/owl";
+import { Layout } from "@web/search/layout";
 
 export class GalleryController extends Component {
     static template = "awesome_gallery.GalleryController";
+    static components = { Layout };
 }
