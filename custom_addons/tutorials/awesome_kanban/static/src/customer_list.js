0ead287a81695d6c87911a706676db590efb15b9
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-23 12:14:18 +0600

    [ADD] awesome_kanban: load and display customer data via ORM in CustomerList

--- custom_addons/tutorials/awesome_kanban/static/src/customer_list.js
+++ custom_addons/tutorials/awesome_kanban/static/src/customer_list.js
@@ -1,8 +1,18 @@
-import { Component } from '@odoo/owl';
+import { Component, useState, onWillStart } from '@odoo/owl';
+import { useService } from '@web/core/utils/hooks';
 
 export class CustomerList extends Component {
     static template = 'awesome_kanban.CustomerList';
     static props = {
         selectCustomer: { type: Function },
     };
+
+    setup() {
+        this.orm = useService('orm');
+        this.state = useState({ customers: [] });
+        onWillStart(async () => {
+            const customers = await this.orm.searchRead('res.partner', [], ['display_name']);
+            this.state.customers = customers;
+        });
+    }
 }
