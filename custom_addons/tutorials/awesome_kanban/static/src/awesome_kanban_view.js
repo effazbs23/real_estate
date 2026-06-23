d85dc910bd557c9baa331078718745d946e96f36
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-23 12:14:31 +0600

    [ADD] awesome_kanban: implement customer filtering in kanban controller via searchModel

--- custom_addons/tutorials/awesome_kanban/static/src/awesome_kanban_view.js
+++ custom_addons/tutorials/awesome_kanban/static/src/awesome_kanban_view.js
@@ -3,11 +3,29 @@ import { kanbanView } from '@web/views/kanban/kanban_view';
 import { registry } from '@web/core/registry';
 import { CustomerList } from './customer_list';
 
+const AWESOME_KANBAN_KEY = Symbol('isFromAwesomeKanban');
+
 class AwesomeKanbanController extends KanbanController {
     static components = {
         ...KanbanController.components,
         CustomerList,
     };
+
+    selectCustomer(partnerId, partnerName) {
+        const customerFilters = this.env.searchModel.getSearchItems(
+            (searchItem) => searchItem[AWESOME_KANBAN_KEY]
+        );
+        for (const customerFilter of customerFilters) {
+            if (customerFilter.isActive) {
+                this.env.searchModel.toggleSearchItem(customerFilter.id);
+            }
+        }
+        this.env.searchModel.createNewFilters([{
+            description: partnerName,
+            domain: [['partner_id', '=', partnerId]],
+            [AWESOME_KANBAN_KEY]: true,
+        }]);
+    }
 }
 
 export const awesomeKanbanView = {
