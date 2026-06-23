4b850a3848c36b2f080124e89641d1c56f6d9963
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-23 12:14:48 +0600

    [ADD] awesome_kanban: add active customers checkbox filter to CustomerList

--- custom_addons/tutorials/awesome_kanban/static/src/customer_list.js
+++ custom_addons/tutorials/awesome_kanban/static/src/customer_list.js
@@ -9,10 +9,25 @@ export class CustomerList extends Component {
 
     setup() {
         this.orm = useService('orm');
-        this.state = useState({ customers: [] });
+        this.state = useState({
+            customers: [],
+            displayActiveCustomers: false,
+        });
         onWillStart(async () => {
-            const customers = await this.orm.searchRead('res.partner', [], ['display_name']);
+            const customers = await this.orm.searchRead(
+                'res.partner', [], ['display_name', 'opportunity_ids']
+            );
             this.state.customers = customers;
         });
+    }
+
+    get customerList() {
+        let customers = this.state.customers;
+        if (this.state.displayActiveCustomers) {
+            customers = customers.filter(
+                (c) => c.opportunity_ids && c.opportunity_ids.length > 0
+            );
+        }
+        return customers;
     }
 }
