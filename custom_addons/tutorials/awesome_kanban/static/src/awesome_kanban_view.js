df9c6696d19feaeabfe749530e5e4c639f78b6ee
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-23 12:13:40 +0600

    [ADD] awesome_kanban: create custom kanban controller and register new kanban view

--- custom_addons/tutorials/awesome_kanban/static/src/awesome_kanban_view.js
+++ custom_addons/tutorials/awesome_kanban/static/src/awesome_kanban_view.js
@@ -1,1 +1,12 @@
-// TODO: Define here your AwesomeKanban view
+import { KanbanController } from '@web/views/kanban/kanban_controller';
+import { kanbanView } from '@web/views/kanban/kanban_view';
+import { registry } from '@web/core/registry';
+
+class AwesomeKanbanController extends KanbanController {}
+
+export const awesomeKanbanView = {
+    ...kanbanView,
+    Controller: AwesomeKanbanController,
+};
+
+registry.category('views').add('awesome_kanban', awesomeKanbanView);
