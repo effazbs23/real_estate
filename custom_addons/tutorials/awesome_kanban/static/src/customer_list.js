38a87762819113860ebcf36f268ab1822dcd69ae
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-23 12:15:42 +0600

    [ADD] awesome_kanban: add pager to CustomerList for customer pagination

--- custom_addons/tutorials/awesome_kanban/static/src/customer_list.js
+++ custom_addons/tutorials/awesome_kanban/static/src/customer_list.js
@@ -1,12 +1,14 @@
 import { Component, useState, onWillStart } from '@odoo/owl';
 import { useService } from '@web/core/utils/hooks';
 import { fuzzyLookup } from '@web/core/utils/search';
+import { Pager } from '@web/core/pager/pager';
 
 export class CustomerList extends Component {
     static template = 'awesome_kanban.CustomerList';
     static props = {
         selectCustomer: { type: Function },
     };
+    static components = { Pager };
 
     setup() {
         this.orm = useService('orm');
@@ -14,7 +16,9 @@ export class CustomerList extends Component {
             customers: [],
             displayActiveCustomers: false,
             searchString: '',
+            offset: 0,
         });
+        this.limit = 20;
         onWillStart(async () => {
             const customers = await this.orm.searchRead(
                 'res.partner', [], ['display_name', 'opportunity_ids']
@@ -23,7 +27,7 @@ export class CustomerList extends Component {
         });
     }
 
-    get customerList() {
+    get filteredCustomers() {
         let customers = this.state.customers;
         if (this.state.displayActiveCustomers) {
             customers = customers.filter(
@@ -34,5 +38,16 @@ export class CustomerList extends Component {
             customers = fuzzyLookup(customers, this.state.searchString, (c) => c.display_name);
         }
         return customers;
+    }
+
+    get displayedCustomers() {
+        return this.filteredCustomers.slice(
+            this.state.offset,
+            this.state.offset + this.limit
+        );
+    }
+
+    onPagerUpdate(newOffset) {
+        this.state.offset = newOffset;
     }
 }
