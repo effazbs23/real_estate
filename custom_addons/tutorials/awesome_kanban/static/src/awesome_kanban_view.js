408368ef503ca6cfa25fe67db63ac31a9113e0c9
effaz rayhan <effaz.rayhan@brainstation-23.com>
2026-06-23 12:13:54 +0600

    [ADD] awesome_kanban: create CustomerList component and extend KanbanView template

--- custom_addons/tutorials/awesome_kanban/static/src/awesome_kanban_view.js
+++ custom_addons/tutorials/awesome_kanban/static/src/awesome_kanban_view.js
@@ -1,8 +1,14 @@
 import { KanbanController } from '@web/views/kanban/kanban_controller';
 import { kanbanView } from '@web/views/kanban/kanban_view';
 import { registry } from '@web/core/registry';
+import { CustomerList } from './customer_list';
 
-class AwesomeKanbanController extends KanbanController {}
+class AwesomeKanbanController extends KanbanController {
+    static components = {
+        ...KanbanController.components,
+        CustomerList,
+    };
+}
 
 export const awesomeKanbanView = {
     ...kanbanView,
